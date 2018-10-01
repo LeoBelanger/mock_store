e7a2ab777997269e207bd0b1df500d05af02c001
BennyLo <bennylojf@gmail.com>
2018-10-01 12:12:57 -0700

    Added first product object

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
+var products = {
+	Box1_$10: {label: "Box 1",
+			   imageUrl: "images/Box1_$10.png",
+			   price: 10,
+			   quantity: 5,
+			}
+};
+
+var test = Object.create( products );
+
+
+console.log(test.Box1_$10);
+
 function Store(initialStock) {	
 	this.stock = initialStock;
 	this.cart = [];
+	this.addItemToCart = function() {
+	}
+	this.removeItemFromCart = function() {
+	}
 }
